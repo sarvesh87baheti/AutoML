6b0fe5b83b022274597d5ff5781d011de53c5566
AkankshaMini37 <kasepogu_a@cs.iitr.ac.in>
2025-11-16 17:35:05 +0530

    To get final best model

---
+++ main/final_model_selection/final_model_sel.py
@@ -0,0 +1,26 @@
+def score_model(metrics, weights=None):
+    if weights is None:
+        weights = {"mse": 0.1, "rmse": 0.3, "mae": 0.2, "r2": 0.4}
+
+    mse = metrics["val"]["mse"]
+    rmse = metrics["val"]["rmse"]
+    mae = metrics["val"]["mae"]
+    r2 = metrics["val"]["r2"]
+
+    score = (
+        weights["mse"] * (1 / mse) +
+        weights["rmse"] * (1 / rmse) +
+        weights["mae"] * (1 / mae) +
+        weights["r2"] * (r2)
+    )
+    return score
+
+
+def compute_model_scores(results):
+    model_scores = {}
+    for model_name, info in results.items():
+        score = score_model(info["metrics"])
+        model_scores[model_name] = score
+
+    best_model = max(model_scores, key=model_scores.get)
+    return model_scores, best_model
